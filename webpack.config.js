bdc64e4c4863722e85763f508c8741128fecf514
Cvanzy <cao.zhi@husky.neu.edu>
2018-12-01 18:00:09 -0500

    heroku set!"

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
 module.exports = (env) => {
   const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
@@ -9,44 +8,41 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
     module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: CSSExtract.extract({
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
             }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      })
-    }]
+          ]
+        })
+      }]
     },
-
     plugins: [
       CSSExtract
     ],
-
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-  }
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
-
